02a90b0aedabf826e71d42ace371469a1e29efaf
Lucas Da Silva <lucasilva15@live.com>
2023-05-30 19:23:37 -0400

    added coke can image and reformated some text

--- src/App.jsx
+++ src/App.jsx
@@ -19,14 +19,19 @@ function App() {
         </div>
       </header>
       <div className='contentContainer'>
-        <div>
-          <h1 className='mainHeader'> Years of refreshing the world, Coca-Cola </h1>
+        <div className='leftsidecontentcontainer'>
+          <div>
+            <h1 className='mainHeader'> Years of refreshing the world, Coca-Cola </h1>
+          </div>
+          <div>
+            <p className='contentHeader'>Coca-Cola, or Coke, is a carbonated soft drink manufactured by the Coca-Cola Company. In 2013, Coke products were sold in over 200 countries worldwide, with consumers drinking more than 1.8 billion company beverage servings each day.</p>
+          </div>
+          <div>
+            <button id='btn3'className='btn'> Buy Now</button>
+          </div>
         </div>
-        <div>
-        <p className='contentHeader'>Coca-Cola, or Coke, is a carbonated soft drink manufactured by the Coca-Cola Company. In 2013, Coke products were sold in over 200 countries worldwide, with consumers drinking more than 1.8 billion company beverage servings each day.</p>
-        </div>
-        
-        <button> Buy Now</button>
+      
+      <img src='https://clipart-library.com/image_gallery2/Coca-Cola-PNG-Picture.png'></img>
       </div>
     </div>
     </>
